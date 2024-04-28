d1c6f61b37366333cd877ed152ac010ff30d320e
rezwan-hossen <rezwanhossen625@gmail.com>
2024-04-28 15:45:57 +0600

    My list show data

--- src/Components/Pages/MyList/MyList.jsx
+++ src/Components/Pages/MyList/MyList.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import useAuth from "../../../Hook/useAuth";
 const MyList = () => {
+  const { user } = useAuth();
+  const [lists, setlists] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5001/tourspot/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setlists(data);
+      });
+  }, []);
   return (
     <div>
       <div className=" text-center">
@@ -18,19 +31,21 @@ const MyList = () => {
               <th>Actions</th>
             </tr>
           </thead>
-          <tr>
-            <td>asdfgfgt</td>
-            <td>asdfgfgt</td>
-            <td>asdfgfgt</td>
-            <td className=" flex gap-2">
-              <button className=" btn bg-green-300">
-                <FiEdit />
-              </button>
-              <button className=" btn bg-red-400">
-                <MdDelete />
-              </button>
-            </td>
-          </tr>
+          {lists.map((list) => (
+            <tr key={list._id}>
+              <td>{list.spotName}</td>
+              <td> {list.country}</td>
+              <td>{list.location} </td>
+              <td className=" flex gap-2">
+                <button className=" btn bg-green-300">
+                  <FiEdit />
+                </button>
+                <button className=" btn bg-red-400">
+                  <MdDelete />
+                </button>
+              </td>
+            </tr>
+          ))}
         </table>
       </div>
     </div>
